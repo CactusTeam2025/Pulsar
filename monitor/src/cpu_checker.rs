d1806fdbe7e1e66084347db90ac8acf1270dc54a
danila <Cactus22032025@gmail.com>
2025-06-21 23:33:56 +0300

    Переписана функция для проверки нагруженности процессора

--- monitor/src/cpu_checker.rs
+++ monitor/src/cpu_checker.rs
@@ -1,22 +1,25 @@
-use sysinfo::{System};
+use sysinfo::{System, Component};
 use std::{thread, time::Duration};
 
 pub fn cpu_checker() {
     let mut sys = System::new();
     
     loop {
+// Очищаем консоль перед началом
+    print!("\x1B[2J\x1B[1;1H");
+
         sys.refresh_cpu_all(); // Обновляем всю информацию о CPU (включая usage)
         
-        println!("Number of physical cores: {}", System::physical_core_count().unwrap_or(0));
-        println!("Number of logical cores: {}", sys.cpus().len());
+        let physical_cores = System::physical_core_count().unwrap_or(0); // Получаем количество физических ядер
+        let logical_cores = sys.cpus().len(); // Получаем количество логических ядер
+        let total_usage = sys.global_cpu_usage(); // Получаем общую загрузку CPU
+        let usage_for_each_core: Vec<f32> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect(); // Получаем использование для каждого ядра
 
-        // Выводим использование для каждого ядра
-        for (i, cpu) in sys.cpus().iter().enumerate() {
-            println!("Core {} ({}): {:.1}% usage", i, cpu.name(), cpu.cpu_usage());
-        }
-
-        // Общая загрузка CPU
-        println!("Total CPU usage: {:.1}%", sys.global_cpu_usage());
+        print!("\x1B[1;1H"); // Перемещаем курсор в начало
+        println!("Number of physical cores: {}", physical_cores);
+        println!("Number of logical cores: {}", logical_cores);
+        println!("Per core usage: {:.1?}", usage_for_each_core);
+        println!("Total CPU usage: {:.1}%", total_usage);
         
         thread::sleep(Duration::from_secs(1));
     }
@@ -24,5 +27,5 @@ pub fn cpu_checker() {
 
 // pub fn temperature_checker()
 // {   
-
+    
 // }
